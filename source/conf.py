676db98bcc0ab5907b0e187156993a54dc841bcb
Peter M <peter@magnusson.institute>
2022-01-18 23:01:24 -0800

    sync

---
+++ source/conf.py
@@ -0,0 +1,19 @@
+# Snackabra Documentation Sphinx File
+# Copyright (c) Magnusson Institute
+
+project = 'snackabra'
+copyright = '2019-2022, Magnusson Institute'
+author = 'Magnusson Institute'
+
+# The full version, including alpha/beta/rc tags
+release = '0.1'
+
+extensions = ['sphinxcontrib.blockdiag', 'sphinxcontrib.seqdiag']
+
+templates_path = ['_templates']
+
+exclude_patterns = []
+
+html_theme = 'furo'
+
+html_static_path = ['_static']
